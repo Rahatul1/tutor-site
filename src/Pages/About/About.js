66a394c74393e9ad22768e7798a47bd9f417b98c
Rahatul <rahatulislam9090@gmail.com>
2022-04-18 03:26:53 +0600

    äbout post set

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,24 @@
 import React from "react";
+import img from "../../images/img1.jpg";
+import "./about.css";
 
 const About = () => {
   return (
-    <div>
-      <h2>about</h2>
+    <div className="about-content p-5">
+      <div
+        className="card mx-auto justify-center align-items-center rounded border shadow "
+        style={{ width: "18rem" }}
+      >
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Rahat Ul</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <button className="btn btn-primary">Check My Profile</button>
+        </div>
+      </div>
     </div>
   );
 };
